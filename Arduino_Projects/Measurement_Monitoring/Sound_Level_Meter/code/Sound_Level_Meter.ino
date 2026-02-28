017fc32ac261935421d545a6f2c2f46043908c04
ErSudhanshuShukla <er.sudhanshushukla@gmail.com>
2026-02-28 13:42:31 +0530

    Update Sound_Level_Meter.ino

--- Arduino_Projects/Measurement_Monitoring/Sound_Level_Meter/code/Sound_Level_Meter.ino
+++ Arduino_Projects/Measurement_Monitoring/Sound_Level_Meter/code/Sound_Level_Meter.ino
@@ -10,15 +10,11 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// =============================
 // LCD Initialization
-// =============================
 // I2C Address: 0x27 (change to 0x3F if not working)
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// =============================
 // Pin Definition
-// =============================
 int mic = A0;   // Microphone analog output connected to A0
 
 void setup() {
@@ -39,20 +35,14 @@ void setup() {
 
 void loop() {
 
-  // =============================
   // Read Microphone Value
-  // =============================
   int raw = analogRead(mic);   // Read analog signal (0–1023)
 
-  // =============================
   // Convert to Approximate dB
-  // =============================
   // Mapping 0–1023 → 30–120 dB (approximation only)
   int dB = map(raw, 0, 1023, 30, 120);
 
-  // =============================
   // Display on LCD
-  // =============================
   lcd.clear();
 
   lcd.setCursor(0, 0);
